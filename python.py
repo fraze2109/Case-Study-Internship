25a69f940a4308d3395bec281695f05c14b85659
fraze2109 <140939156+fraze2109@users.noreply.github.com>
2023-07-30 19:13:54 +0800

    case_study.py

--- python.py
+++ python.py
@@ -1,22 +1,28 @@
+# import json (for output)
 import json
 
 
+# function to generate array
 def generate_array():
-    result = []
+    result = []  # create array
+    # iterate from 1 to 100 using for loop, 
+    # add each number into the array except for those that fits the criteria
     for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
+        if num % 3 == 0 and num % 5 == 0:  # if divisible by 3 and 5, add BIGBANG into array
             result.append("BIGBANG")
-        elif num % 3 == 0:
+        elif num % 3 == 0:  # if divisible by 3, add BIG into array
             result.append("BIG")
-        elif num % 5 == 0:
+        elif num % 5 == 0:  # if divisible by 5, add BANG into array
             result.append("BANG")
-        else:
+        else:  # if does not fit any of the above, add the number into the array
             result.append(str(num))
     return result
 
-def save_to_json(output_array):
-    with open('output.json', 'w') as file:
-        json.dump(output_array, file)
+#  function to generate .json file for output
+def save_to_json(output):
+    with open('result.json', 'w') as file:
+        json.dump(output, file)
 
-output_array = generate_array()
-save_to_json(output_array)
+#  call functions
+output = generate_array()
+save_to_json(output)
